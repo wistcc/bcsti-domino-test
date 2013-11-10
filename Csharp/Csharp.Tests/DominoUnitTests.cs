b2152dc5c622af86e201817296514b1ed882d994
amhed <aherrera@gmail.com>
2013-11-10 17:15:45 -0400

    Configuracion Inicial Proyecto

---
+++ Csharp/Csharp.Tests/DominoUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Csharp.Tests
+{
+    [TestClass]
+    public class DominoUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
