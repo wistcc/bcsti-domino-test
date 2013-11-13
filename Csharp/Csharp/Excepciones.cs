debce759721d930526bf68171dc750f951c09279
amhed <aherrera@gmail.com>
2013-11-13 10:00:13 -0400

    Agregando excepciones nuevas

---
+++ Csharp/Csharp/Excepciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Csharp.Excepciones
+{
+    public class JuegoNoComenzoConDobleSeisException : Exception
+    {
+    }
+
+    public class JugadorNoPuedePasarConFichasException : Exception
+    {
+        
+    }
+
+    public class JugadorIntentoJugarFichaQueNoPoseeException : Exception
+    {
+    }
+
+    public class JugadorIntentoJugarEnTurnoErroneoException : Exception
+    {}
+}
